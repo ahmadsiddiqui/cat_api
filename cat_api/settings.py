b6218068dd3f68239072c0755816bfa73fbe839e
git <git@github.com>
2026-09-15 22:40:51 -0700

    [Deploy] Removed secrets for deployment

--- cat_api/settings.py
+++ cat_api/settings.py
@@ -13,20 +13,17 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-9&qj645p0(=0-*4--^dg$kh90erdl9k+bpb188klml(jh3s0c4')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get("DEBUG")==True
 ALLOWED_HOSTS = ['cat.asid.dev','localhost']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
