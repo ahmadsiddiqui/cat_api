4ca794cbda7e60734c2ffbff565c80303dc9a821
git <git@github.com>
2026-09-21 21:56:45 -0700

    fixes

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ for ((i=0; i<length; i++)); do
     result+="${chars:RANDOM%${#chars}:1}"
 done
 echo "$result"
-echo "DEBUG = False \n SECRET_KEY=$result" > ./cat_api/.env
+echo -e "DEBUG = False \n SECRET_KEY=$result" > ./cat_api/.env
 git add .
 git commit -m "$1"
 git push origin master
 render deploys create srv-dajm90h594qs73coloq0 --confirm --wait
-echo "DEBUG = False \n SECRET_KEY=$result" > ./cat_api/.env
+echo -e "DEBUG = True \n SECRET_KEY=$result" > ./cat_api/.env
